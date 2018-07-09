7388390617b02712454abb363caa68d33e9a14e7
Blanca <blancacc2.71828@gmail.com>
2018-07-09 11:25:44 +0200

    Creo presentación 2 y añado código con dropbox

---
+++ pruebas_dash/main.py
@@ -0,0 +1,99 @@
+# Basic dash library
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+# import csv
+import pandas as pd
+
+# plot dessing
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly import tools
+
+
+#open data file, columns' mean  votage,current,time,capacity
+ 
+df = pd.read_csv('df.csv' , header=None)
+
+# Get values grahp
+voltage = go.Scatter(
+    y = df[0],
+    x = df [2],
+    name = 'voltage'
+)
+current = go.Scatter(
+    y = df[1],
+    x = df [2],
+    name = 'current'
+)
+
+capacity = go.Scatter(
+    y=df [3],
+    x=df [2],
+    name = 'capacity'
+ )
+
+fig_voltage = tools.make_subplots()
+fig_voltage.append_trace( voltage , 1 ,1)
+fig_voltage['layout'].update(height=600, width=600, title='voltage')
+
+fig_current = tools.make_subplots()
+fig_current.append_trace( current , 1 ,1)
+fig_current['layout'].update(height=600, width=600, title='current')
+
+fig_capacity = tools.make_subplots()
+fig_capacity.append_trace( capacity , 1 ,1)
+fig_capacity['layout'].update(height=600, width=600, title='capacity')
+
+# Tests representes
+# T1	High Vacuum test
+# T2	Capacity and Internal Resistance vs Temperature
+# T3	Self-Discharge Test
+# T4	LEO Cycling
+# T5	EMF vs SOC
+# T6	Reduced pressure 30 % DOD Cycling
+# T7	Reduced pressure 80% DOD Cycling
+
+#Start graph
+app = dash.Dash()
+app.layout = html.Div([
+    html.H1('Results'),
+    # Select test
+    html.Label('Test selection:'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'High vacuum test', 'value': 't1'},
+            {'label': 'Capacity and Internal Resistance vs Temperature', 'value': 't2'},
+            {'label': 'Self-Discharge', 'value': 't3'},
+            {'label': 'LEO Cycling', 'value': 't4'},
+            {'label': 'LEO Cycling', 'value': 't5'},
+            {'label': 'Reduced pressure 30 % DOD Cycling', 'value': 't6'},
+            {'label': 'Reduced pressure 80% DOD Cycling', 'value': 't7'}
+            
+        ],
+        value='MTL'
+    ),
+    html.Div([
+        html.Div([
+            html.H3('Voltage'),
+            dcc.Graph(id='g1', figure=fig_voltage)
+        ], className="six columns"),
+
+        html.Div([
+            html.H3('Current'),
+            dcc.Graph(id='g2', figure=fig_current)
+        ], className="six columns"),
+             
+        html.Div([
+            html.H3('Capacity'),
+            dcc.Graph(id='g3', figure=fig_capacity)
+        ], className="six columns"),
+    ], className="row")
+])
+
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
